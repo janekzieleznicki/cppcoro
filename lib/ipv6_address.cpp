f0ecd7de149c07be2a20460c47a91359f30da172
Janek Zieleźnicki <janek.zieleznicki@gmail.com>
2020-10-17 21:17:54 +0200

    cmake build

--- lib/ipv6_address.cpp
+++ lib/ipv6_address.cpp
@@ -356,7 +356,7 @@ std::string cppcoro::net::ipv6_address::to_string() const
 		}
 	}
 
-	assert((c - &buffer[0]) <= sizeof(buffer));
+	assert((c - &buffer[0]) <= static_cast<long>(sizeof(buffer)));
 
 	return std::string{ &buffer[0], c };
 }
